8561610ede6d1ee3ac02f45f4e2938391f887d74
Franck Siguier <franck.siguier@gmail.com>
2021-01-14 17:07:29 +0100

    API to store restaurants with comments related to them

---
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -0,0 +1,36 @@
+class Api::V1::RestaurantsController < Api::V1::BaseController
+  before_action :set_restaurant, only: [:show, :update]
+  acts_as_token_authentication_handler_for User, except: [:index, :show ]
+
+  def index
+    @restaurants = policy_scope(Restaurant)
+    authorize @restaurants
+  end
+
+  def show
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
+  end
+
+  def render_error
+    render json: { errors: @restaurant.errors.full_messages },
+      status: :unprocessable_entity
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant  # For Pundit
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Destroying database'
+
+User.destroy_all
+Restaurant.destroy_all
+Comment.destroy_all
+
+puts 'Creating new seeds'
+
+mat = User.create!(email:'test@test.com', password:'123456')
+franck = User.create!(email:'test1@test.com', password:'123456')
+
+rest1 = Restaurant.create!(name:'le beau cochon', address:'78 rue sully', user_id: mat.id)
+rest2 = Restaurant.create!(name:'le bon cochon', address:'27 rue sala', user_id: mat.id)
+
+comment1 = Comment.create!(user_id: franck.id, restaurant_id: rest1.id, content:"C'est vraiment très bon")
+comment1 = Comment.create!(user_id: franck.id, restaurant_id: rest2.id, content:"C'est vraiment très bon ici aussi")
